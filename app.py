e928d30ccc013a22b9625d355d2ddffe93b1d0f8
sasundharan <144417404+sasundharan@users.noreply.github.com>
2026-05-12 15:49:18 +0530

    app.py

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ app = Flask(__name__)
 
 # Initialize MediaPipe Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5)
-
+face_mesh = mp_face_mesh.FaceMesh(
+    static_image_mode=True,
+    max_num_faces=1,
+    refine_landmarks=True
+)
 # Hairstyle Recommendations with External Image URLs
 men_face_shapes = {
     "Oval": [
